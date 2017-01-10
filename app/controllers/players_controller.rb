308460199fbd5cc0a8bae66ae257e212121fac4f
Dogukan <dogukan@Dogukans-MacBook-Pro.local>
2017-01-10 22:24:20 +0300

    Player controller, model and some views added

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,45 @@ class PlayersController < ApplicationController
   def index
   	@players = Player.all
   end
+
+  def new
+  	@player = Player.new
+  end
+
+  def create
+  	@player = Player.new(player_params)
+    if @player.save
+      flash[:info] = "Player has been successfully created."
+      redirect_to @player
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  	@player = Player.find(params[:id])
+  end
+
+  def edit
+  	@player = Player.find(params[:id])
+  end
+
+  def update
+  	@player = Player.find(params[:id])
+    if @player.update_attributes(player_params)
+      flash[:success] = "Profile updated"
+      redirect_to @player
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  	
+  end
+
+  private
+    def player_params
+      params.require(:player).permit(:username, :firstname, :lastname, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'players/index'
+  root                  'players#index'
+  resources :players,      only: [:edit, :update, :show, :create, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
